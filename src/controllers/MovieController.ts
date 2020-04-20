1cbf6093e34a8137924d89a9af683892b7be0136
Florian Kapfenberger <florian@kapfenberger.me>
2020-04-20 22:31:54 +0200

    change TS setup and templating engine

--- src/controllers/MovieController.ts
+++ src/controllers/MovieController.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from 'express';
 import fetch from 'node-fetch';
 
-const API_KEY = '';
+const API_KEY = process.env.TMDB_KEY;
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const tmdb = {
   movies: (): Promise<any> => {
     return fetch(
       `${BASE_URL}movie/popular?api_key=${API_KEY}&page=1`
-    ).then(res => res.json());
+    ).then((res) => res.json());
   },
 };
 
@@ -17,15 +17,18 @@ interface Movie {
 
 const MovieController = {
   index: async (_req: Request, res: Response) => {
-    const movieResponse = await tmdb.movies();
-
-    const movies = movieResponse.results.map((movie: Movie) => {
-      return {
-        ...movie,
-        poster_path: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
-      };
-    });
-    res.render('movie/index', { movies });
+    try {
+      const movieResponse = await tmdb.movies();
+      const movies = movieResponse.results.map((movie: Movie) => {
+        return {
+          ...movie,
+          poster_path: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+        };
+      });
+      res.render('movie', { movies });
+    } catch (error) {
+      res.send('error ' + error.message);
+    }
   },
 };
 
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,21 @@
 import express from 'express';
 import path from 'path';
-import mustacheExpress from 'mustache-express';
-
-import './styles/index.css';
+import exphbs from 'express-handlebars';
 
 import MovieController from './controllers/MovieController';
 
 const app = express();
 const port = 3000;
+const viewPath = path.join(__dirname, 'views');
 
-app.engine('html', mustacheExpress());
-app.set('view engine', 'html');
-app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+app.set('views', viewPath);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', MovieController.index);
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log('App is listening on port 3000!');
 });
