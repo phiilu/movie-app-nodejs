5e32083b588a6dd2ab9d1ab79745d7cd21088ba2
Florian Kapfenberger <florian@kapfenberger.me>
2020-05-02 18:10:53 +0200

    update build script

--- build.sh
+++ build.sh
@@ -7,3 +7,5 @@ tsc
 echo 'coping assets to dist folder'
 cp -R src/views dist/
 cp -R src/public dist/
+echo 'building tailwindcss...'
+NODE_ENV=production npx postcss-cli src/styles/index.css -o dist/public/styles/index.css
