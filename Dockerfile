94324ddb8ea86999ccd9fe402b9fce0bf1c66ba9
Florian Kapfenberger <florian@kapfenberger.me>
2020-05-02 18:25:59 +0200

    fix tailwind purge config

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:12 as build
 RUN mkdir -p /app
 WORKDIR /app
-COPY package.json package-lock.json tsconfig.json build.sh /app/
+COPY tailwind.config.js postcss.config.js package.json package-lock.json tsconfig.json build.sh /app/
 COPY src /app/src
 RUN npm ci
 RUN npm run build
--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 rm -rf dist
 echo 'cleaned dist folder'
+echo 'compiling Tailwind...'
+NODE_ENV=production npx postcss-cli src/styles/index.css -o dist/public/styles/index.css
 echo 'compiling TypeScript...'
 tsc
 echo 'coping assets to dist folder'
 cp -R src/views dist/
 cp -R src/public dist/
-echo 'building tailwindcss...'
-NODE_ENV=production npx postcss-cli src/styles/index.css -o dist/public/styles/index.css
