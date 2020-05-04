65275ab543eb268bd79f38df42cacec5033c21cc
Florian Kapfenberger <florian@kapfenberger.me>
2020-05-04 21:54:04 +0200

    remove console.log

--- src/controllers/SearchController.ts
+++ src/controllers/SearchController.ts
@@ -4,7 +4,6 @@ import api from '../api';
 const SearchController = {
   search: async (req: Request, res: Response) => {
     const term: string = <string>req.query.term;
-    console.log(term);
     if (!term) {
       res.status(400).json({ message: 'No search term provided' });
       return;
